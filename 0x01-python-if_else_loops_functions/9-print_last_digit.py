5cfa3a9226b4e825e59765b0f40a25e824295c03
Juan Diego Alejandro Valencia Pena <47123111+Jdavp@users.noreply.github.com>
2019-05-01 00:25:13 -0500

    "{}"

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,5 +4,5 @@ def print_last_digit(number):
         number = number * -1
     if number >= 10:
         number = number % 10
-    print({}.format(number), end="")
+    print("{}".format(number), end="")
     return number
