2fe3c4c5009d4b73b9420f34704e291815816245
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-05-02 23:05:05 +0000

    first 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    strfinal = ""
+    for l in str:
+        if ord(l) >= 97 and ord(l) <= 122:
+            strfinal += (chr(ord(l) - 32))
+        else:
+            strfinal +=l
+    print(strfinal)
