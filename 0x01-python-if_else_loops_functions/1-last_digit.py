05e5c519ec58625f21c22c0b919724943c4b51bb
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-05-02 21:15:13 +0000

    taking out spaces

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 string = "Last digit of "
-greater = " and is greater than 5 "
-zero = " and is 0 "
-less = " and is less than 6 and not 0 "
+greater = " and is greater than 5"
+zero = " and is 0"
+less = " and is less than 6 and not 0"
 lastdigit = 0
 if number > 0:
     lastdigit = number % 10
