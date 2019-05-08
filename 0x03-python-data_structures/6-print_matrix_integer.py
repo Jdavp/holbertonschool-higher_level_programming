355eff89b77e1618b3dd023d851978036cbfc296
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-05-08 02:39:03 +0000

    many changes

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,11 +3,10 @@ def print_matrix_integer(matrix=[[]]):
     if matrix is not None:
         if matrix != [[]]:
             columns = len(matrix)
-            rows = len(matrix[0])
-            for y in range(rows):
-                for x in range(columns - 1):
+            for y in range(columns):
+                for x in range(len(matrix[y]) - 1):
                     print("{:d}".format(matrix[y][x]), end=' ')
-                    x += 1
-                print('{:d}'.format(matrix[y][x]))
+                x += 1
+                print("{:d}".format(matrix[y][x]))
         else:
             print()
