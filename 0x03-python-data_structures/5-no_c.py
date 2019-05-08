d1cb97897e19442d55cd7af1256ff5e6c586ece7
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-05-08 01:01:45 +0000

    no space cC

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if my_string is not None:
-        return (my_string.translate({ord(i): None for i in 'c, C'}))
+        return (my_string.translate({ord(i): None for i in 'cC'}))
