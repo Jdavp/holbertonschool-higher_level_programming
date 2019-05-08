4d75833a3b78900f0b77359c7eaae7ad4891153b
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-05-08 00:31:26 +0000

    first 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list is not None:
+        if(idx < len(my_list) and idx >= 0):
+            new_list = my_list.copy()
+            new_list[idx] = element
+            return(new_list)
+        else:
+            return(my_list)
