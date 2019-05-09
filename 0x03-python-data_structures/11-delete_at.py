64108311c406569a3e7818e549ed0f8047820ae4
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-05-09 03:22:50 +0000

    no new list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    nueva = my_list
     if idx < 0 and idx > (len(my_list)):
         return(my_list)
     else:
-        del nueva[idx]
-        return(nueva)
+        del my_list[idx]
+        return(my_list)
