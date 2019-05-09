dc81c2136e4fc3e201ae2ee599daa0917e4d6dc4
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-05-09 00:24:54 +0000

    return largo none

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     largo = len(sentence)
-    if sentence is not None:
-        return(largo, sentence[0])
-    elif largo == 0:
+    if largo == 0:
         return(largo, None)
+    else:
+        return(largo, sentence[0])
