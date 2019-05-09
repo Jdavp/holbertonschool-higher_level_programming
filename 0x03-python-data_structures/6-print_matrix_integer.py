a9688946d77b387f2ed4fac89da5d1528e099a3a
Jdavp <juandiegoalejandro@hotmail.com>
2019-05-09 05:11:12 +0000

    new logic

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is not None:
-        if matrix != [[]]:
-            columns = len(matrix)
-            for y in range(columns):
-                for x in range(len(matrix[y]) - 1):
-                    print("{:d}".format(matrix[y][x]), end=' ')
-                x += 1
-                print("{:d}".format(matrix[y][x]))
-        else:
-            print()
+    for row in matrix:
+        for item in row:
+            print("{:d}".format(item), end=" ")
+        print("")
