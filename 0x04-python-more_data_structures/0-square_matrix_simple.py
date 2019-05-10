2e9dd7bc99554b36eb281eed07a62618b643ba5a
Jdavp <juandiegoalejandro@hotmail.com>
2019-05-10 02:32:54 +0000

    first 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    nueva = []
+    for x in matrix:
+        segunda = []
+        for item in x:
+            segunda.append(item * item)
+        nueva.append(segunda)
+    return('{}'.format(nueva))
