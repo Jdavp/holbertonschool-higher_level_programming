cbe2b30320c2f0e1f661ef34fe256dfbe72a7ac6
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-05-21 01:50:06 +0000

    first 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    if True:
+        try:
+            print('{:d}'.format(value))
+            return (True)
+        except BaseException as a:
+            print("Exception: {}".format(a), file=sys.stderr)
+            return (False)
+            pass
