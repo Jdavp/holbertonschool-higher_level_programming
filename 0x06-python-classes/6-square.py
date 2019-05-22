6e8df2799f8d27419b2774e1f1fb36a64c10f5a0
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-05-22 03:45:21 +0000

    change order of propertys

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -9,6 +9,11 @@ class Square:
     def size(self):
         return self.__size
 
+    @property
+    def position(self):
+        return self.__position
+
+
     @size.setter
     def size(self, value):
             if type(value) is not int:
@@ -16,10 +21,6 @@ class Square:
             if value < 0:
                 raise ValueError('size must be >= 0')
             self.__size = value
-
-    @property
-    def position(self):
-        return self.__position
 
     @position.setter
     def position(self, value):
