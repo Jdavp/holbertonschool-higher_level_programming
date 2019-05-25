a5598b46b5a9ef5b267a07bea71eba85d5f615a0
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-05-25 05:35:56 +0000

    fixes to unicode

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,16 +5,16 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-  def test_max(self)
-  """Test
-  """
-
-  self.assertEqual(max_integer([1, 2, 3, 5]), 5)
-  self.assertEqual(max_integer(), None)
-  self.assertEqual(max_integer([1, 2, 3.5]), 3.5)
-  self.assertEqual(max_integer(["hola", "amigo"), "hola")
-  self.assertRaises(TypeError, max_integer, [1, "hola"])
-  self.assertRaises(TypeError, max_integer, None)
+  def test_max(self):
+    """Test
+    """
+    # Test max integer
+    self.assertEqual(max_integer([1, 2, 3, 5]), 5)
+    self.assertEqual(max_integer(), None)
+    self.assertEqual(max_integer([1, 2, 3.5]), 3.5)
+    self.assertEqual(max_integer(["hola", "amigo"]), "hola")
+    self.assertRaises(TypeError, max_integer, [1, "hola"])
+    self.assertRaises(TypeError, max_integer, None)
 
 if __name__ == '__main__':
     unittest.main()
