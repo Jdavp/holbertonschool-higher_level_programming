3dad8bdd92d755a07e14f4cdfb67efe920574812
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-05-28 17:09:26 +0000

    change to 2 lines

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -62,4 +62,5 @@ class Rectangle:
 
     def __repr__(self):
         "method to return a string representation"
-        return "Rectangle(" + str(self.width) + ',' + str(self.height) + ")"
+        return "Rectangle(" + str(self.__width) + ','\
+            + str(self.__height) + ")"
