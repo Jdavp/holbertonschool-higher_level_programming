801861a288743c1a13198b6f07bdcec1edc649b7
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-06-06 02:26:05 +0000

    first 10

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''unction that returns the dictionary description with simple data structure'''
+
+
+def class_to_json(obj):
+    '''initializing object'''
+    return (obj.__dict__)
