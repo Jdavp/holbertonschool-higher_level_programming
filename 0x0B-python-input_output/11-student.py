ce2a9fc839dcd017d46ba04798c2a21b520055c8
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-06-06 02:40:12 +0000

    with comments

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -3,10 +3,13 @@
 
 
 class Student:
+    '''declaring a class student'''
     def __init__(self, first_name, last_name, age):
+        '''initial class variables'''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        '''method to return a dictionary of the object'''
         return self.__dict__
