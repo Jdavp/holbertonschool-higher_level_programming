cb7816016d58d474cef89c298b47bea66e56ff9e
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-07-17 01:19:41 +0000

    with inner join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT cities.id, cities.name, states.name FROM cities ,states ORDER BY cities.id 
+SELECT cities.id,cities.name,states.name FROM cities INNER JOIN states ON states.id = cities.id  
