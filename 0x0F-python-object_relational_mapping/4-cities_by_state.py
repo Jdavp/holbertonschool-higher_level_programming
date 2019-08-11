49a9ba8ef9aaf8564c328872dfc55d3b161cfdaf
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-08-11 02:48:41 +0000

    add import argv

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 'Write a script that lists all cities from the database hbtn_0e_0_usa'
 import MySQLdb
+from sys import argv
 if __name__ == '__main__':
 
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
