704f5264047900b74771205d39d6f701a8e92998
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-08-11 02:42:20 +0000

    fix format

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,12 +3,12 @@
 import MySQLdb
 from sys import argv
 if __name__ == '__main__':
-
+    'make a list of all states'
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
 
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE BYNARY name = %s", (argv[4],))
+    cur.execute("SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC".format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
