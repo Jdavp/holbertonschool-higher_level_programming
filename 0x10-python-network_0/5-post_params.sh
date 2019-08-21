aca74dc40b7856691887a5e38eb2d97aff07db37
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-08-21 00:18:52 +0000

    add s1

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a POST request to the passed URL, and displays the body of the response#
-curl -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -X POST https://flaviocopes.com/
+curl -d "$1" "email=hr@holbertonschool.com&subject=I will always be here for PLD" -X POST https://flaviocopes.com/
