5a48b44765e612df8e6b7b0961537fe3d2677cf1
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-08-26 22:33:35 +0000

    [fix]format tabs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,8 @@ import urllib.request
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
-print("""Body response:
-    - type: {}
-    - content: {}
-    - utf8 content: {}""".format(type(html), html, (html.decode("utf8"))))
+print("""Body response:"""'\n'
+      '\t'"""- type: {}"""'\n'
+      '\t'"""- content: {}"""'\n'
+      '\t'"""- utf8 content: {}"""
+      .format(type(html), html, (html.decode("utf8"))))
