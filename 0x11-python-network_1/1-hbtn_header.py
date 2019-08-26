03e2b2ff4938625abce48530d0fae5661ae31e77
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-08-26 23:45:21 +0000

    add double execution protection

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 'script that fetches https://intranet.hbtn.io/status'
-import urllib.request
-from sys import argv
+if __name__ == "__main__":
+    import urllib.request
+    from sys import argv
 
 with urllib.request.urlopen(argv[1]) as response:
     headers = response.info()
