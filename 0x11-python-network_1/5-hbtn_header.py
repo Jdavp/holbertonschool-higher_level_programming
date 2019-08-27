d2b060d96f680b1a5770432c5363940a4ff197c7
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-08-27 03:50:15 +0000

    after pep8

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'sends a request to the URL and displays the value of the variable X-Request-Id'
+'request to the URL and displays the value of the variable X-Request-Id'
 if __name__ == "__main__":
     import requests
     from sys import argv
