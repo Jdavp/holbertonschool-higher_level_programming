8eef53c360316d89ac53873b1ccff283d736f203
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-08-27 03:38:02 +0000

    change to r.text

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     r = requests.get('https://intranet.hbtn.io/status')
     print("""Body response:"""'\n'
           '\t'"""- type: {}"""'\n'
-          '\t'"""- content: {}"""'\n'
+          '\t'"""- content: {}"""
           .format(type(r.text), r.text))
