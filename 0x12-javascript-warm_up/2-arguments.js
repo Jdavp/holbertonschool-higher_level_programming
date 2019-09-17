7a33ebebdfe60253614896d67cabbc1c873a1094
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-09-17 22:53:02 +0000

    first 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 // prints a message depending of the number of arguments passed
-if (process.argv.length > 2) {
+if (process.argv.length === 2) {
   console.log('Argument found');
+} else if (process.argv.length > 3) {
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
