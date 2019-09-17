178e45a54047d9a35e4def1c5a1e95f21f80ca8e
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-09-17 23:03:43 +0000

    fix comparision

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // prints a message depending of the number of arguments passed
-if (process.argv.length === 2) {
+if (process.argv.length === 3) {
   console.log('Argument found');
-} else if (process.argv.length > 3) {
+} else if (process.argv.length >= 4) {
   console.log('Arguments found');
 } else {
   console.log('No argument');
