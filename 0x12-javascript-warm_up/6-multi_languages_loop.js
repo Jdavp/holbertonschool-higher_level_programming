40e55f44efcc72f492a056fb0f54e2a44313c7b3
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-09-18 00:41:27 +0000

    first 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// prints 3 lines: by using an array of string and a loop
+let i = 0;
+const language = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+while (i < language.length) {
+  console.log(language[i]);
+  i++;
+}
