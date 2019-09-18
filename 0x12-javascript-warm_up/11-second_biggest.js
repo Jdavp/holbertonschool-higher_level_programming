96b9ea4acd263bf29f9bed5f45e5a175a0ae9cc4
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-09-18 03:33:23 +0000

    first 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// script that prints x times “C is fun”
+const i = (process.argv.slice(2));
+let lista;
+if (i.length > 1) {
+  lista = i.sort();
+  console.log(lista.slice(-2)[0]);
+} else {
+  console.log(0);
+}
