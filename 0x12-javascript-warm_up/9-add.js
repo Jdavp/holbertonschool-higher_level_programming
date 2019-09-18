adce866d5b48399b3f0cd7f7c47e8f4abca9f1bf
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-09-18 02:13:27 +0000

    first 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// script that prints x times “C is fun”
+function add (a, b) {
+  if (parseInt(a) && parseInt(b)) {
+    console.log(parseInt(a) + parseInt(b));
+  } else {
+    console.log(NaN);
+  }
+}
+
+add(process.argv[2], process.argv[3]);
