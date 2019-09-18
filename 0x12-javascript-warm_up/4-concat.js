013ce8b6f0e728cca2dd8bda4cae97dad8b8e9c8
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-09-18 00:15:45 +0000

    fix logic

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 // script that prints the first argument passed to it
-if (process.argv[1] && process.argv[2]) {
-  console.log('undefined is undefined');
-} else if (process.argv[2] && process.argv[3]) {
-  console.log(process.argv[2] + process.arvg[3]);
+if (process.argv[2]) {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
 } else {
   console.log(process.argv[2] + ' is ' + process.argv[3]);
 }
