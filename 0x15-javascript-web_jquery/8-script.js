3c8a1a389fcb3bb64f2db8cd8df4f9e8f787158f
Juan Diego Alejandro Valencia Pena <juadiegoalejandro@hotmail.com>
2019-09-26 20:27:25 -0500

    first 8 and  9

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,6 @@
+$.get( "https://swapi.co/api/films/?format=json", function( data ) {
+        data.results.forEach(dict => { 
+        $( "#list_movies" )
+        .append( "<li>"+dict.title+"</li>" );
+            })
+  }, "json" );
---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,4 @@
+$.get( "https://fourtonfish.com/hellosalut/?lang=fr", function( data ) {
+        $( "#hello" )
+        .append( data.hello );
+  }, "json" );
